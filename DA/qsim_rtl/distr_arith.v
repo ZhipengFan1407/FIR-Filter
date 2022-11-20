9ec50c9b02506074edab925e2dcfae41de0e1eef
Matthew Stridiron <mstridiron.ptech@gmail.com>
2022-11-20 11:50:18 -0500

    Add files via upload

--- DA/qsim_rtl/distr_arith.v
+++ DA/qsim_rtl/distr_arith.v
@@ -1,6 +1,9 @@
 `timescale 1ns/1ps
-
-
+`define SD #0.010
+`define HALF_CLOCK_PERIOD #5
+`define QSIM_OUT_FN "./qsim.out"
+`define INPUT_DATA "./input.results"
+`define OUTPUT_DATA "./out.results"
 
 
 module distr_arith (
@@ -38,6 +41,16 @@ module distr_arith (
     wire [31 : 0] dff0_out;
     wire counter_reach;
 
+	integer input_file;
+	integer qsim_out_file;
+	integer out_file;
+	integer i;
+	integer ret_read;
+
+        reg signed [20:0] out_file_reg;
+        reg signed [20:0] lfsr_out_qsim;
+        reg signed [20:0] input_out_file;
+
     // Look-Up Table Instantiation
 
     // Adder Instantiation
@@ -59,5 +72,91 @@ module distr_arith (
 
     // Counter Instantiation
     counter counter0 (.reset(reset), .clk(clk3), .count_reach(count_reach));
+
+
+        always begin
+                `HALF_CLOCK_PERIOD;
+                clk3 = ~clk3;
+        end
+
+
+	initial begin
+
+                input_file = $fopen(`INPUT_DATA, "rb"); //LOAD IN THE INPUT LUT DATA
+                if(!input_file) begin
+                $display("Couldn't open the input file.");
+                $finish;
+                end
+
+                out_file = $fopen(`OUTPUT_DATA, "rb"); //LOAD IN OUTPUT FILE TO COMPARE ANSWERS WITH
+                if(!matlab_out_file) begin
+                $display("Couldn't open the output file.");
+                $finish;
+                end
+
+
+                qsim_out_file = $fopen(`QSIM_OUT_FN, "w"); //LOAD A QSIM FILE SO WE CAN WRITE TO IT
+                if(!qsim_out_file) begin
+                $display("Couldn't create the output file.");
+                $finish;
+                end
+
+
+		clk3 <= 0;
+
+		@(posedge clk);
+
+                for(i = 0; i < 2; i=i+1) begin
+                $display("i: %0d", i);
+
+                ret_read = $fscanf(input_file, "%0d\n", LUT0_out);
+                $display("LUT0: %0d", LUT0_out);
+                ret_read = $fscanf(input_file, "%0d\n", LUT1_out);
+                $display("LUT1: %0d", LUT1_out);
+
+                ret_read = $fscanf(input_file, "%0d\n", LUT2_out);
+                $display("LUT2: %0d", LUT2_out);
+                ret_read = $fscanf(input_file, "%0d\n", LUT3_out);
+                $display("LUT3: %0d", LUT3_out);
+
+
+                ret_read = $fscanf(input_file, "%0d\n", LUT4_out);
+                $display("LUT4: %0d", LUT4_out);
+                ret_read = $fscanf(input_file, "%0d\n", LUT5_out);
+                $display("LUT5: %0d", LUT5_out);
+
+
+                ret_read = $fscanf(input_file, "%0d\n", LUT6_out);
+                $display("LUT6: %0d", LUT6_out);
+                ret_read = $fscanf(input_file, "%0d\n", LUT7_out);
+                $display("LUT7: %0d", LUT7_out);
+
+                //compare with results from the simulation
+                ret_read = $fscanf(out_file,"%0d\n",out_file_reg);
+                $display("output:%0d", out_file_reg);
+		
+//posedge clocks here
+@(posedge clk);
+@(posedge clk);
+@(posedge clk);
+@(posedge clk);
+@(posedge clk);
+@(posedge clk);
+@(posedge clk);
+@(posedge clk);	
+
+                lfsr_out_qsim = S;
+                $display("s: %0d", S);
+		$fwrite(qsim_out_file, "%0d\n", lfsr_out_qsim);
+
+		end
+	
+		$fclose(qsim_out_file);
+		$finish;
+
+	end
+
+
+
 
 endmodule
