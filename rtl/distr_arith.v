690411953b0f872ccce1f085c3480e00a626954a
Zhipeng Fan <richiefzp@gmail.com>
2022-11-20 16:00:25 -0500

    Update distr_arith.v

--- rtl/distr_arith.v
+++ rtl/distr_arith.v
@@ -3,19 +3,18 @@
 module distr_arith (
     input clk3,
     input reset,
-    input [7 : 0] x1_bit,   // 0~7
-    input [7 : 0] x2_bit,   // 8~15
-    input [7 : 0] x3_bit,   // 16~23
-    input [7 : 0] x4_bit,   // 24~31
-    input [7 : 0] x5_bit,   // 32~39
-    input [7 : 0] x6_bit,   // 40~47
-    input [7 : 0] x7_bit,   // 48~55
-    input [7 : 0] x8_bit,   // 56~63
+    input [7 : 0] x0_bit,   // 0~7
+    input [7 : 0] x1_bit,   // 8~15
+    input [7 : 0] x2_bit,   // 16~23
+    input [7 : 0] x3_bit,   // 24~31
+    input [7 : 0] x4_bit,   // 32~39
+    input [7 : 0] x5_bit,   // 40~47
+    input [7 : 0] x6_bit,   // 48~55
+    input [7 : 0] x7_bit,   // 56~63
     output reg [31 : 0] sum
 );
 
     // Wire and Bus Definition
-    // Make sure 16-bit LUT append with 16 zeros to the left
     wire [31 : 0] LUT0_out;
     wire [31 : 0] LUT1_out;
     wire [31 : 0] LUT2_out;
@@ -37,6 +36,14 @@ module distr_arith (
     wire counter_reach;
 
     // Look-Up Table Instantiation
+    lut0 lookup0 (.addr(x7_bit), .data_out(LUT0_out));
+    lut1 lookup1 (.addr(x6_bit), .data_out(LUT1_out));
+    lut2 lookup2 (.addr(x5_bit), .data_out(LUT2_out));
+    lut3 lookup3 (.addr(x4_bit), .data_out(LUT3_out));
+    lut4 lookup4 (.addr(x3_bit), .data_out(LUT4_out));
+    lut5 lookup5 (.addr(x2_bit), .data_out(LUT5_out));
+    lut6 lookup6 (.addr(x1_bit), .data_out(LUT6_out));
+    lut7 lookup7 (.addr(x0_bit), .data_out(LUT7_out));
 
     // Adder Instantiation
     adder add0 (.in_a(LUT0_out), .in_b(LUT1_out), .out(add0_out));
